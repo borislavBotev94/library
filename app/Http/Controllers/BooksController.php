322ef550d7dc372b77e1c81b4992730c4e3a29ab
Borislav Botev <borislav.botev@webtrade.bg>
2019-07-25 15:20:14 +0300

    Book Test

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Book;
 
 class BooksController extends Controller
 {
 
 
-    public function store(){
 
+    public function store(){
+        Book::create($this->validateRequest());
+    }
+
+
+    public function update(Book $book){
+        $book->update($this->validateRequest());
+    }
+
+    /**
+     * @return mixed
+     */
+    public function validateRequest()
+    {
+        return request()->validate([
+            'title' => 'required',
+            'author' => 'required'
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+Route::post('/books','BooksController@store');
+Route::patch('/books/{book}','BooksController@update');
