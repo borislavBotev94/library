eaa0b36a4b6556405988d7fcd5812e88c2204dda
Borislav Botev <borislav.botev@webtrade.bg>
2019-07-25 17:07:33 +0300

    Test Driven Laravel - e02

--- tests/Feature/BookManagementTest.php
+++ tests/Feature/BookManagementTest.php
@@ -14,15 +14,15 @@ class BookReservationTest extends TestCase
     public function a_book_can_be_added_to_the_library()
     {
 
-        $this->withoutExceptionHandling();
          $response = $this->post('/books',[
              'title' => 'Cool Book Title',
              'author' => 'Victor',
          ]);
 
 
-         $response->assertOk();
          $this->assertCount(1, Book::all());
+         $book = Book::first();
+         $response->assertRedirect($book->path());
     }
 
 
@@ -54,21 +54,46 @@ class BookReservationTest extends TestCase
     /** @test */
     public function a_book_can_be_updated(){
 
-        $this->withoutExceptionHandling();
         $this->post('/books',[
             'title' => 'Cool Title',
             'author' => 'Victor',
         ]);
 
         $book = Book::first();
-        $response = $this->patch('/books/'.$book->id,[
+        $response = $this->patch($book->path(),[
             'title' => 'New Title',
             'author' => 'New Author',
         ]);
 
         $this->assertEquals('New Title', Book::first()->title);
         $this->assertEquals('New Author', Book::first()->author);
+
+
+        $response->assertRedirect($book->fresh()->path());
     }
 
+
+
+    /** @test */
+
+
+    public function a_book_can_be_deleted()
+    {
+ 
+        $this->post('/books',[
+            'title' => 'Cool Title',
+            'author' => 'Victor',
+        ]);
+
+        $book = Book::first();
+
+        $this->assertCount(1,Book::all());
+        $response = $this->delete($book->path());
+        $this->assertCount(0,Book::all());
+
+
+        $response->assertRedirect('/books');
+
+    }
 
 }
